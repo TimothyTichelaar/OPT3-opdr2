621e7bf10fc398120e4dd8b3b540d93807b6b065
TimothyTichelaar <21108366@student.hhs.nl>
2022-05-31 15:33:08 +0200

    BoormachineFactory aangemaakt in ProductFactory

--- src/ProductFactory.java
+++ src/ProductFactory.java
@@ -1,7 +1,7 @@
 public abstract class ProductFactory {
     public static final PersonenautoFactory PERSONENAUTO_FACTORY = new PersonenautoFactory();
     public static final VrachtautoFactory VRACHTAUTO_FACTORY = new VrachtautoFactory();
-    public static final BoormachineFactory BOORMACHINE_FACTORY;
+    public static final BoormachineFactory BOORMACHINE_FACTORY = new BoormachineFactory();
 
     public abstract Product maakProduct ();
 }
