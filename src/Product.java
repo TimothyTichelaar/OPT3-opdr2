4e3f23e0040fe1410c4100784fed58d04b5a36db
TimothyTichelaar <21108366@student.hhs.nl>
2022-05-31 14:41:11 +0200

    Van de interface een abstract class gemaakt.

--- src/Product.java
+++ src/Product.java
@@ -1,3 +1,3 @@
-public interface Product {
-    double prijsPerDag(boolean verzekering, int leeftijd);
+public abstract class Product {
+    abstract double prijsPerDag(boolean verzekering, int leeftijd);
 }
