46b9977c2b6b65e1be177e6324456a8d0c5cc3ac
TimothyTichelaar <21108366@student.hhs.nl>
2022-06-01 11:50:57 +0200

    Instance variabele van product public gemaakt zodat ze door de verschillende classen van roducten gebruikt kunnen worden.

--- src/Product.java
+++ src/Product.java
@@ -1,39 +1,14 @@
 public abstract class Product {
     //-----Instance variabelen-----
-    private double huurprijsPerDag;
-    private double verzekeringPerDag;
-    private boolean verhuurd;
+    public double huurprijsPerDag;
+    public double verzekeringPerDag;
+    public boolean verhuurd;
 
     //-----Constructor-----
-    public Product (double huurprijsPerDag, double verzekeringPerDag, boolean verhuurd) {
-        this.huurprijsPerDag = huurprijsPerDag;
-        this.verzekeringPerDag = verzekeringPerDag;
-        this.verhuurd = verhuurd;
-    }
 
     //-----Get methoden-----
-    public double getHuurprijsPerDag() {
-        return this.huurprijsPerDag;
-    }
-
-    public double getVerzekeringPerDag() {
-        return this.verzekeringPerDag;
-    }
-
-    public boolean getVerhuurd() { return this.verhuurd; }
 
     //-----Set methoden
-    public void setHuurprijsPerDag(double huurprijsPerDag) {
-        this.huurprijsPerDag = huurprijsPerDag;
-    }
-
-    public void setVerzekeringPerDag(double verzekeringPerDag) {
-        this.verzekeringPerDag = verzekeringPerDag;
-    }
-
-    public void setVerhuurd(boolean verhuren) {
-        this.verhuurd = verhuren;
-    }
 
     //-----Overige methoden-----
     abstract double prijsPerDag(boolean verzekering, int leeftijd);
