b0a86d981bfbfa9a172a8e34801bf0a6f1cc9106
TimothyTichelaar <21108366@student.hhs.nl>
2022-06-22 12:07:52 +0200

    juiste button aangeroepen en redundent code verwijderd

--- src/main/java/opdr2/MenuController.java
+++ src/main/java/opdr2/MenuController.java
@@ -1,19 +1,13 @@
 package opdr2;
 
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
-
-import java.io.IOException;
-import java.util.Objects;
 
 public class MenuController extends SceneController {
 
-    public javafx.scene.control.Button uitloggen;
+    @FXML Button uitloggen;
 
     public void closeButtonAction(ActionEvent actionEvent){
         SceneController.ingelogd.remove(medewerker);
